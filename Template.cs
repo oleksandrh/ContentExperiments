0c4bba7f4593ea6fc0875760857b3d9060a4ba46
oleksandrh <alex.hrebeniuk@gmail.com>
2019-01-17 16:10:57 +0200

    Create Template.cs

---
+++ Template.cs
@@ -0,0 +1,71 @@
+public class Template
+{
+    public int Id { get; set; }
+    public Feature[] Features { get; set; }
+  	
+  	public Template() 
+    {
+      this.Id = 1;
+      this.Features = new []
+      {
+        new Feature() {
+           Id = 1,
+           FeatureType = new FeatureType() { Id = 1, Name = "Header", IsContainer = false },
+           PropertiesSet = new PropertiesSet 
+           { 
+             Id = 1, 
+             Text = "Some text for header", 
+             BackgroundColor = "#ffffff", 
+             Font = "Arial, Helvetica, sans-serif",
+             ChildFeaturesAlign = "vertical"
+           },
+           Features = null
+        },
+        new Feature() {
+          Id = 2,
+          FeatureType = new FeatureType() { Id = 2, Name = "Products", IsContainer = true },
+          PropertiesSet = new PropertiesSet
+          {
+            Id = 2, 
+            ChildFeaturesAlign = "vertical"
+          },
+          Features = new Feature[]
+          {
+            new Feature() {
+              Id = 3, 
+              FeatureType = new FeatureType() { Id = 3, Name = "Product", IsContainer = false },
+          	  PropertiesSet = new PropertiesSet
+              {
+            	Id = 3, 
+            	Text = "Some product name"
+          	  },
+              Features = null
+            }
+          }
+        }
+      };
+  	}
+}
+
+public class Feature
+{
+    public int Id { get; set; }
+    public FeatureType FeatureType { get; set; }
+    public PropertiesSet PropertiesSet { get; set; }
+  	public Feature[] Features { get; set; }
+}
+
+public class FeatureType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsContainer { get; set; }
+}
+
+public class PropertiesSet {
+  	public int Id { get; set; }
+  	public string BackgroundColor { get; set; }
+    public string Text { get; set; }
+  	public string Font { get; set; }
+  	public string ChildFeaturesAlign { get; set; }
+}
