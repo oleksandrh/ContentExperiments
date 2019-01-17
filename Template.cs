82aef1e41bb7120ca60fb86496bcfd314bc99735
oleksandrh <alex.hrebeniuk@gmail.com>
2019-01-17 16:17:29 +0200

    Update Template.cs

--- Template.cs
+++ Template.cs
@@ -2,49 +2,52 @@ public class Template
 {
     public int Id { get; set; }
     public Feature[] Features { get; set; }
-  	
-  	public Template() 
+
+    public Template()
     {
-      this.Id = 1;
-      this.Features = new []
-      {
-        new Feature() {
-           Id = 1,
-           FeatureType = new FeatureType() { Id = 1, Name = "Header", IsContainer = false },
-           PropertiesSet = new PropertiesSet 
-           { 
-             Id = 1, 
-             Text = "Some text for header", 
-             BackgroundColor = "#ffffff", 
-             Font = "Arial, Helvetica, sans-serif",
-             ChildFeaturesAlign = "vertical"
-           },
-           Features = null
-        },
-        new Feature() {
-          Id = 2,
-          FeatureType = new FeatureType() { Id = 2, Name = "Products", IsContainer = true },
-          PropertiesSet = new PropertiesSet
-          {
-            Id = 2, 
-            ChildFeaturesAlign = "vertical"
-          },
-          Features = new Feature[]
-          {
-            new Feature() {
-              Id = 3, 
-              FeatureType = new FeatureType() { Id = 3, Name = "Product", IsContainer = false },
-          	  PropertiesSet = new PropertiesSet
-              {
-            	Id = 3, 
-            	Text = "Some product name"
-          	  },
-              Features = null
+        Id = 1;
+        Features = new[]
+        {
+            new Feature()
+            {
+                Id = 1,
+                FeatureType = new FeatureType() { Id = 1, Name = "Header", IsContainer = false },
+                PropertiesSet = new PropertiesSet
+                {
+                    Id = 1,
+                    Text = "Some text for header",
+                    BackgroundColor = "#ffffff",
+                    Font = "Arial, Helvetica, sans-serif",
+                    ChildFeaturesAlign = "vertical"
+                },
+               Features = null
+            },
+            new Feature()
+            {
+                Id = 2,
+                FeatureType = new FeatureType() { Id = 2, Name = "Products", IsContainer = true },
+                PropertiesSet = new PropertiesSet
+                {
+                    Id = 2,
+                    ChildFeaturesAlign = "vertical"
+                },
+                Features = new Feature[]
+                {
+                    new Feature()
+                    {
+                        Id = 3,
+                        FeatureType = new FeatureType() { Id = 3, Name = "Product", IsContainer = false },
+                        PropertiesSet = new PropertiesSet
+                        {
+                            Id = 3,
+                            Text = "Some product name"
+                        },
+                        Features = null
+                    }
+                }
             }
-          }
-        }
-      };
-  	}
+        };
+    }
 }
 
 public class Feature
@@ -52,7 +55,7 @@ public class Feature
     public int Id { get; set; }
     public FeatureType FeatureType { get; set; }
     public PropertiesSet PropertiesSet { get; set; }
-  	public Feature[] Features { get; set; }
+    public Feature[] Features { get; set; }
 }
 
 public class FeatureType
@@ -62,10 +65,11 @@ public class FeatureType
     public bool IsContainer { get; set; }
 }
 
-public class PropertiesSet {
-  	public int Id { get; set; }
-  	public string BackgroundColor { get; set; }
+public class PropertiesSet
+{
+    public int Id { get; set; }
+    public string BackgroundColor { get; set; }
     public string Text { get; set; }
-  	public string Font { get; set; }
-  	public string ChildFeaturesAlign { get; set; }
+    public string Font { get; set; }
+    public string ChildFeaturesAlign { get; set; }
 }
